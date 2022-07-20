93fe8cd59c83766c38a7279ca5797bf22300bc77
qfourca <qfourca3305@gmail.com>
2022-07-20 12:32:32 +0900

    real end

--- text-socket/app.js
+++ text-socket/app.js
@@ -1,5 +1,7 @@
 var app = require("express")();
 var express = require("express");
+const { fdatasync } = require("fs");
+const { getSystemErrorName } = require("util");
 var server = require("http").createServer(app);
 // http server를 socket.io server로 upgrade한다
 var io = require("socket.io")(server, { path: '/socket.io' });
@@ -12,14 +14,13 @@ app.get("/", function (req, res) {
 });
 
 const names = []
-// connection event handler
-// connection이 수립되면 event handler function의 인자로 socket인 들어온다
 io.on("connection", function (socket) {
-  // 접속한 클라이언트의 정보가 수신되면
   socket.on("login", function (data) {
+    socket.emit("all", {names: names})
+    console.log(names)
+    data.name = getName(data.name)
     names.push(data.name)
     socket.name = data.name;
-    socket.emit("all", {names: names})
     socket.broadcast.emit("login", {name: data.name});
   });
 
@@ -39,17 +40,7 @@ io.on("connection", function (socket) {
       msg: data.msg,
     };
 
-    // 메시지를 전송한 클라이언트를 제외한 모든 클라이언트에게 메시지를 전송한다
-    socket.broadcast.emit("move", msg);
-
-    // 메시지를 전송한 클라이언트에게만 메시지를 전송한다
-    // socket.emit("s2c chat", msg);
-
-    // 접속된 모든 클라이언트에게 메시지를 전송한다
-    // io.emit("s2c chat", msg);
-
-    // 특정 클라이언트에게만 메시지를 전송한다
-    // io.to(id).emit('s2c chat', data);
+    socket.broadcast.emit("chat", msg);
   });
 
 
@@ -72,3 +63,13 @@ io.on("connection", function (socket) {
 server.listen(3000, function () {
   console.log("Socket IO server listening on port 3000");
 });
+
+function getName(str) {
+  if(names.includes(str)) {
+    str = str + '1'
+    return getName(str)
+  }
+  else {
+    return str
+  }
+}
